1a94e20121c8fb57da7d006949855ea37c2afc82
Rexon9040 <150456454+Rexon9040@users.noreply.github.com>
2023-11-12 16:42:24 +0530

    Update Streamlit_app.py

--- Streamlit_app.py
+++ Streamlit_app.py
@@ -1,3 +1,8 @@
 import streamlit
 
 streamlit.title('My parent Lunch plan')
+
+Streamlit.header ('Lunch menu')
+Streamlit.text('Dal & Rice')
+Streamlit.text('Paneer & Roti')
+streamlit.text('Boiled Egg & chapati with muttan curry')
